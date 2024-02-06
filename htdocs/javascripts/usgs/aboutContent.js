a07b3736165a4ca61f32d7d58eeb347055f0f04b
Leonard Orzol <154551929+llorzol@users.noreply.github.com>
2024-02-06 13:34:41 -0800

    version 2.02 February 6, 2024
    Minor remove of 2017 from copyright

--- htdocs/javascripts/usgs/aboutContent.js
+++ htdocs/javascripts/usgs/aboutContent.js
@@ -9,7 +9,7 @@
 
 /*
 ###############################################################################
-# Copyright (c) 2017 Oregon Water Science Center
+# Copyright (c) Oregon Water Science Center
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
