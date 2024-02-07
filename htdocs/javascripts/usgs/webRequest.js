86b7d322904481492a62450f4482ca844d7c3a4a
Leonard Orzol <154551929+llorzol@users.noreply.github.com>
2024-02-06 19:35:00 -0800

    version 1.09 November 15, 2016

--- htdocs/javascripts/usgs/webRequest.js
+++ htdocs/javascripts/usgs/webRequest.js
@@ -9,7 +9,7 @@
 
 /*
 ###############################################################################
-# Copyright (c) 2016 Oregon Water Science Center
+# Copyright (c) Oregon Water Science Center
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
